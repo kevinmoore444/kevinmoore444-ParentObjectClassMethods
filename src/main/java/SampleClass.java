b720b1ecc676bc86f7c3b55eaf6dedd207dbb8d8
Kevin Moore <kevinmoore444@gmail.com>
2023-06-05 15:34:16 +0000

    Completed object class methods exercise.

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -55,10 +55,19 @@ public class SampleClass {
     public int a;
     public boolean b;
 
-    //    implement a custom .equals(SampleClass other){} method here.
+    public boolean equals(SampleClass other){
+        if(this.a == other.a && this.b == other.b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
 
-    //    implement a custom .toString(){} method here.
+    public String toString(){
+        return "Printing out this objects attributes: " + this.a + ", " + this.b;
+    }
 
     
 }
